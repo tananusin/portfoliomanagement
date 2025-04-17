41b65c3e3b98feccf4f4983c001ffc32613eabfa
tananusin <tananusin@gmail.com>
2025-04-17 17:15:50 +0700

    Update positionsize.py

--- positionsize.py
+++ positionsize.py
@@ -1,8 +1,8 @@
 # positionsize.py
 
-def classify_position(weight_pct, target_pct, threshold=0.05): # Defaul threshold 5%
+def classify_position(weight_pct, target_pct, threshold=0.50): # Defaul %drift threshold 50%
     # Calculate the drift
-    drift = weight_pct - target_pct
+    drift = ((weight_pct - target_pct) / target_pct)
     
     # Check if the position is oversized, undersized, or aligned
     if drift > threshold:
