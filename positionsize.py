782fae65cd97b8bab2e4b61567266a464ed99989
tananusin <tananusin@gmail.com>
2025-04-17 18:07:42 +0700

    Update positionsize.py

--- positionsize.py
+++ positionsize.py
@@ -2,19 +2,20 @@
 
 import pandas as pd
 
-def classify_position(weight_pct, target_pct, threshold=0.50): # Defaul %drift threshold 50%
+def classify_position(weight_pct, target_pct, drift_threshold=0.05, drift_pct_threshold=0.50):
     # Check if target is set
     if pd.isna(weight_pct) or pd.isna(target_pct) or target_pct == 0:
         drift = 0
-        return "-", drift
+        return "-"
     
-    # Calculate the drift    
-    drift = (weight_pct - target_pct) / target_pct
+    # Calculate raw drift and percentage drift
+    drift = weight_pct - target_pct
+    drift_pct = drift / target_pct
 
     # Check if the position is oversized, undersized, or aligned
-    if drift > threshold:
-        return "oversize", drift
-    elif drift < -threshold:
-        return "undersize", drift
+    if drift > drift_threshold or drift_pct > drift_pct_threshold:
+        return "oversize"
+    elif drift < -drift_threshold or drift_pct < drift_pct_threshold:
+        return "undersize"
     else:
-        return "-", drift
+        return "-"
