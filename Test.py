b099c37c98101ca8338d85ff95a5ff7c075d28fc
T-Nai <tananusin@gmail.com>
2025-04-27 17:09:12 +0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,17 +1,17 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 @dataclass
 class StockData:
-	#Google Sheet Variables
+    # Google Sheet Variables
     name: str
     ticker: str
     currency: str
     shares: float
     target: float
-    type: str
-	
-    #Porfolio Variables
-    Price: float
+    asset_type: str
+
+    # Portfolio Variables
+    price: float
     fx: float
     value_local: float
     value_thb: float
