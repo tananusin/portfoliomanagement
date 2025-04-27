ec1f040047f1c30479066d54757f1fe9c5ff9a26
T-Nai <tananusin@gmail.com>
2025-04-27 23:09:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -38,9 +38,9 @@ portfolio_df = individual_df if show_individual else summarized_df
 # --- Format and Display Table ---
 show_cols = ["name", "symbol", "currency", "shares", "price", "fx rate", "value (thb)", "weight", "target", "type"]
 format_dict = {
-    "shares": "{:,.2f}" if x != 0.0 else "-",
-    "price": "{:,.2f}" if x != 0.0 else "-",
-    "fx rate": "{:,.2f}" if x != 0.0 else "-",
+    "shares": lambda x: f"{x:,.2f}" if x != 0.0 else "-",
+    "price": lambda x: f"{x:,.2f}" if x != 0.0 else "-",
+    "fx rate": lambda x: f"{x:,.2f}" if x != 0.0 else "-",
     "value (thb)": lambda x: f"{x:,.0f}" if x != 0.0 else "-",
     "weight": lambda x: f"{x * 100:.1f}%" if x is not None else "-",
     "target": lambda x: f"{x * 100:.1f}%" if x != 0.0 else "-",
