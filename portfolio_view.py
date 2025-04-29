94fdd6cddcf5ca8436120f360d79f1e25faeca80
T-Nai <tananusin@gmail.com>
2025-04-29 13:30:15 +0700

    Update portfolio_view.py

--- portfolio_view.py
+++ portfolio_view.py
@@ -1,6 +1,8 @@
 # portfolio_view.py
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
 from asset_data import AssetData
 from typing import List
 
@@ -12,3 +14,26 @@ def get_individual_df(assets: List[AssetData]) -> pd.DataFrame:
         "weight": asset.weight,
         "type": asset.asset_type
     } for asset in assets])
+
+def show_portfolio_table(portfolio_df: pd.DataFrame):
+    show_cols = ["name", "weight", "type"]
+    format_dict = {
+        "weight": lambda x: f"{x * 100:.1f}%" if x is not None else "-",
+    }
+    st.dataframe(portfolio_df[show_cols].style.format(format_dict))
+
+def show_allocation_pie_chart(portfolio_df: pd.DataFrame, total_thb: float):
+    st.subheader("📈 Allocation Pie Chart")
+
+    chart_df = portfolio_df[["name", "value (thb)"]].copy()
+    chart_df["weight (%)"] = (chart_df["value (thb)"] / total_thb * 100).round(2)
+    chart_df = chart_df[chart_df["weight (%)"] >= 1]
+
+    fig, ax = plt.subplots()
+    chart_df.set_index("name")["weight (%)"].plot.pie(
+        autopct="%1.0f%%",
+        figsize=(5, 5),
+        ylabel="",
+        ax=ax
+    )
+    st.pyplot(fig)
