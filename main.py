11407f2425278000b9e9aeeb3c5f0bd1d03bc08d
T-Nai <tananusin@gmail.com>
2025-04-29 13:31:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from asset_data import AssetData
 from load_assets import load_assets_from_google_sheet
 from portfolio_value import enrich_assets, calculate_portfolio_total, assign_weights
-from portfolio_view import get_individual_df
+from portfolio_view import get_individual_df, show_portfolio_table, show_allocation_pie_chart
 
 # Streamlit page config
 st.set_page_config(page_title="Portfolio Management", layout="centered")
@@ -25,28 +25,9 @@ with st.spinner("Fetching live prices and FX rates..."):
 # Create DataFrame
 portfolio_df = get_individual_df(assets)
 
-# --- Format and Display Table ---
-show_cols = ["name", "weight", "type"]
-format_dict = {
-    "weight": lambda x: f"{x * 100:.1f}%" if x is not None else "-",
-}
-st.dataframe(portfolio_df[show_cols].style.format(format_dict))
+# --- Display Table ---
+show_portfolio_table(portfolio_df)
 
-# --- Pie Chart ---
-st.subheader("📈 Allocation Pie Chart")
+# --- Display Pie Chart ---
+show_allocation_pie_chart(portfolio_df, total_thb)
 
-chart_df = portfolio_df[["name", "value (thb)"]].copy()
-chart_df["weight (%)"] = (chart_df["value (thb)"] / total_thb * 100).round(2)
-
-# Filter Out <1% Weight Asset
-chart_df = chart_df[chart_df["weight (%)"] >= 1]
-
-# Pie Chart
-fig, ax = plt.subplots()
-chart_df.set_index("name")["weight (%)"].plot.pie(
-    autopct="%1.0f%%",
-    figsize=(5, 5),
-    ylabel="",
-    ax=ax
-)
-st.pyplot(fig)
