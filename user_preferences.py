9b3c2af092a210789ea9f6cf7d0610999e01ef4d
T-Nai <tananusin@gmail.com>
2025-04-29 18:24:01 +0700

    Update user_preferences.py

--- user_preferences.py
+++ user_preferences.py
@@ -9,9 +9,9 @@ def get_user_preferences() -> float:
     # Custom min/max labels
     col1, col2 = st.sidebar.columns(2)
     with col1:
-        st.markdown("<div style='text-align: left;'>25% (Risk-Off)</div>", unsafe_allow_html=True)
+        st.markdown("<div style='text-align: left;'>Risk-Off</div>", unsafe_allow_html=True)
     with col2:
-        st.markdown("<div style='text-align: right;'>75% (Risk-On)</div>", unsafe_allow_html=True)
+        st.markdown("<div style='text-align: right;'>Risk-On</div>", unsafe_allow_html=True)
 
     # Investment % Slider
     investment_pct = st.sidebar.slider(
