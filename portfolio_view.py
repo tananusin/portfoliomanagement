505e26ffaa4a5cff558338e870fe4f65a142d734
T-Nai <tananusin@gmail.com>
2025-04-29 18:07:49 +0700

    Update portfolio_view.py

--- portfolio_view.py
+++ portfolio_view.py
@@ -6,6 +6,22 @@ import streamlit as st
 from asset_data import AssetData
 from typing import List
 
+def get_user_preferences() -> float:
+    st.sidebar.header("User Preference")
+    
+    st.sidebar.markdown("### 🎯 Investment Mode")
+    # Custom min/max labels
+    col1, col2 = st.sidebar.columns(2)
+    with col1: st.markdown("<div style='text-align: left;'>25% (Risk-Off)</div>", unsafe_allow_html=True)
+    with col2: st.markdown("<div style='text-align: right;'>75% (Risk-On)</div>", unsafe_allow_html=True)
+    # Investment % Slider
+    investment_pct = st.sidebar.slider(
+        label="Set Investment Portion (%)",
+        min_value=25, max_value=75, value=50, step=1,
+        help="Investment portion are Core, Growth, and Speculative assets. Reserve portion are Cash, Bond, and Gold."
+    )
+    return investment_pct
+
 def get_individual_df(assets: List[AssetData]) -> pd.DataFrame:
     return pd.DataFrame([{
         "name": asset.name,
