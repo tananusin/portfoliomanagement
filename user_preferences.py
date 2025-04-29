af86b1823501d8da88b96122a5f3f5a01f8ea285
T-Nai <tananusin@gmail.com>
2025-04-29 19:42:38 +0700

    Update user_preferences.py

--- user_preferences.py
+++ user_preferences.py
@@ -4,16 +4,9 @@ import streamlit as st
 
 def get_user_preferences() -> float:
     st.sidebar.header("User Preference")
-    st.sidebar.markdown("### 🎯 Investment Mode")
-
-    # Custom min/max labels
-    col1, col2 = st.sidebar.columns(2)
-    with col1:
-        st.markdown("<div style='text-align: left;'>Risk-Off</div>", unsafe_allow_html=True)
-    with col2:
-        st.markdown("<div style='text-align: right;'>Risk-On</div>", unsafe_allow_html=True)
 
     # Investment % Slider
+    st.sidebar.markdown("### 🎯 Investment Mode: Risk-Off/Risk-On")
     investment_pct = st.sidebar.slider(
         label="Set Investment Portion (%)",
         min_value=25,
@@ -22,5 +15,4 @@ def get_user_preferences() -> float:
         step=1,
         help="Investment portion are Core, Growth, and Speculative assets. Reserve portion are Cash, Bond, and Gold."
     )
-
     return investment_pct
