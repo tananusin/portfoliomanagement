7663095c048be8db38e49c3c242608f5e607f801
T-Nai <tananusin@gmail.com>
2025-04-29 14:33:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pandas as pd
 from asset_data import AssetData
 from load_assets import load_assets_from_google_sheet
 from portfolio_value import enrich_assets, calculate_portfolio_total, assign_weights
+from portfolio_proportion import assign_targets
 from portfolio_view import get_individual_df, show_portfolio_table, show_allocation_pie_chart
 
 # Streamlit page config
@@ -20,6 +21,10 @@ with st.spinner("Fetching live prices and FX rates..."):
     assets = enrich_assets(assets)
     total_thb = calculate_portfolio_total(assets)
     assign_weights(assets, total_thb)
+
+# Dynamic Target Position Size For Each Asset
+investment_pct = st.sidebar.slider("Investment %", min_value=10, max_value=100, value=50, step=1)
+assign_targets(assets, investment_pct)
 
 # Create DataFrame
 portfolio_df = get_individual_df(assets)
