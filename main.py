30cf085bddb893451e88195e230c37b51c27ea32
T-Nai <tananusin@gmail.com>
2025-04-29 18:09:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -22,23 +22,8 @@ with st.spinner("Fetching live prices and FX rates..."):
     total_thb = calculate_portfolio_total(assets)
     assign_weights(assets, total_thb)
 
-# Sidebar
-st.sidebar.header("User Preference")
-st.sidebar.markdown("### 🎯 Investment Mode")
-
-# Custom min/max labels
-col1, col2 = st.sidebar.columns(2)
-with col1: st.markdown("<div style='text-align: left;'>25% (Risk-Off)</div>", unsafe_allow_html=True)
-with col2: st.markdown("<div style='text-align: right;'>75% (Risk-On)</div>", unsafe_allow_html=True)
-# Slider
-investment_pct = st.sidebar.slider(
-    label="Set Investment Portion (%)",
-    min_value=25,
-    max_value=75,
-    value=50,
-    step=1,
-    help="Investment portion are speculative, growth and core assets. Reserve portion are cash, bond and gold."
-)
+# Sidebar for User Preference 
+investment_pct = get_user_preferences()
 
 # Dynamic Target Position Size For Each Asset
 assign_targets(assets, investment_pct)
