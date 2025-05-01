aae98a9040ad42d30d610bbff5b603d004e401ea
T-Nai <tananusin@gmail.com>
2025-05-01 14:40:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from asset_data import AssetData
 from load_assets import load_assets_from_google_sheet
-from portfolio_value import enrich_assets, calculate_portfolio_total, assign_weights
+from portfolio_value import enrich_assets, summarize_assets, calculate_portfolio_total, assign_weights
 from user_preferences import get_user_preferences
 from portfolio_proportion import assign_targets
 from portfolio_view import get_portfolio_df, show_portfolio_table, show_allocation_pie_chart
@@ -22,15 +22,16 @@ password = st.text_input("Enter the password for fetching real-time data:", type
 
 # Validate the password
 if password == st.secrets["credentials"]["app_password"]:
-    st.success("Password correct! Access granted.")
+    st.success("Password correct! Fetching live data...")
     
     # Fetch price, fx, and 
     with st.spinner("Fetching live prices and FX rates..."):
         assets = enrich_assets(assets)
 else:
-    st.error("Use Google Sheet Data.")
+    st.error("Using Google Sheet data without live update.")
     
 # Calculate Portfolio Values
+assets = summarize_assets(assets)
 total_thb = calculate_portfolio_total(assets)
 assign_weights(assets, total_thb)
 
