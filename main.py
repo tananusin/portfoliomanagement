371a84029c8f0c36427c0c26f1e0313db8eecc6e
T-Nai <tananusin@gmail.com>
2025-05-02 21:34:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from portfolio_view import get_portfolio_df, show_portfolio_table, show_allocati
 
 # Streamlit page config
 st.set_page_config(page_title="Portfolio Management", layout="centered")
-st.title("📊 Portfolio Management")
+st.title("🗂️ Portfolio Management")
 
 # Load Google Sheet and Create AssetData objects
 sheet_url = st.secrets["google_sheet"]["url"]
