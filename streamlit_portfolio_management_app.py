751452728bd47165ed49da065c713d193b17c934
T-Nai <tananusin@gmail.com>
2025-05-28 15:42:10 +0700

    Update streamlit_portfolio_management_app.py

--- streamlit_portfolio_management_app.py
+++ streamlit_portfolio_management_app.py
@@ -9,7 +9,7 @@ from portfolio_value import enrich_assets, summarize_assets, calculate_portfolio
 from user_preferences import get_user_preferences, UserPreference
 from portfolio_proportion import assign_targets
 from position_size import assign_position_sizes
-from price_change import assign_price_change
+from price_change import assign_price_changes
 from pe_signal import assign_pe_signals
 from portfolio_view import get_portfolio_df, show_portfolio_table, show_allocation_pie_chart, show_target_allocation_pie_chart
 
@@ -48,7 +48,7 @@ assign_weights(assets, total_thb)
 # --- Assign Dynamic Target and Position ---
 assign_targets(assets, user_pref)
 assign_position_sizes(assets)
-assign_price_change(assets, user_pref)
+assign_price_changes(assets, user_pref)
 
 # --- Assign PE Signal ---
 assign_pe_signals(assets)
