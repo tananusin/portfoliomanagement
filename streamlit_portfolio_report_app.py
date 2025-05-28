f08b38134d5771cfa1dba8a5d0c12fa7df00a178
T-Nai <tananusin@gmail.com>
2025-05-28 13:30:01 +0700

    Update streamlit_portfolio_report_app.py

--- streamlit_portfolio_report_app.py
+++ streamlit_portfolio_report_app.py
@@ -79,7 +79,7 @@ def show_portfolio_table(portfolio_df: pd.DataFrame):
     st.dataframe(portfolio_df[show_cols].style.format(format_dict))
 
 def show_market_data_table(portfolio_df: pd.DataFrame):
-    st.subheader("📋 Portfolio Report (Full Details)")
+    st.subheader("📋 Market Data")
     show_cols = ["name", "currency", "price", "fx rate", "52w high", "52w low", "pe", "yield"]
     format_dict = {
         "price": lambda x: f"{x:,.2f}" if x != 0.0 else "-",
