fe33c3889ee3166b6d0f056f79273576817ca8a4
T-Nai <tananusin@gmail.com>
2025-05-29 18:28:23 +0700

    Update yield_signal.py

--- yield_signal.py
+++ yield_signal.py
@@ -22,6 +22,6 @@ def assign_yield_signals(assets: List[AssetData], prefs: UserPreference) -> List
         if asset.dividend_yield is not None and asset.dividend_yield >= asset.dividend_yield_recovery:
             asset.dividend_yield_signal = "sufficient"
         else:
-            asset.dividend_yield_signal = "insufficient"
+            asset.dividend_yield_signal = "-"
 
     return assets
