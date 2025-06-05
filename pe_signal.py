fc0ca74fd76a8bc60720bba4ea4d26216342a5ef
T-Nai <tananusin@gmail.com>
2025-06-05 11:37:24 +0700

    Update pe_signal.py

--- pe_signal.py
+++ pe_signal.py
@@ -4,8 +4,12 @@ from typing import List
 
 def assign_pe_signals(assets: List[AssetData]) -> None:
     for asset in assets:
-        if asset.pe_ratio is None or asset.pe_p25 is None or asset.pe_p75 is None:
-            asset.pe_signal = None
+        if (
+            asset.pe_ratio is None or asset.pe_ratio == 0 or
+            asset.pe_p25 is None or asset.pe_p25 == 0 or
+            asset.pe_p75 is None or asset.pe_p75 == 0
+        ):
+            asset.pe_signal = "no data"
         elif asset.pe_ratio < asset.pe_p25:
             asset.pe_signal = "undervalue"
         elif asset.pe_ratio > asset.pe_p75:
