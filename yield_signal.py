ededfa6fbcd9a9065163e721f2dfc1c1df3f95e3
T-Nai <tananusin@gmail.com>
2025-06-07 16:06:15 +0700

    Update yield_signal.py

--- yield_signal.py
+++ yield_signal.py
@@ -9,6 +9,7 @@ def assign_yield_signals(assets: List[AssetData], prefs: UserPreference) -> List
         if asset.asset_type is None:
             continue
 
+        # Assign offset based on asset type
         if asset.asset_type == "Speculative":
             asset.dividend_yield_offset = prefs.yield_speculative / 100
         elif asset.asset_type == "Growth":
@@ -19,6 +20,11 @@ def assign_yield_signals(assets: List[AssetData], prefs: UserPreference) -> List
             asset.dividend_yield_signal = None
             continue
 
+        # Adjust offset if drop_1y is more severe
+        if asset.drop_1y is not None and asset.dividend_yield_offset < (asset.drop_1y / -5):
+            asset.dividend_yield_offset = asset.drop_1y / -5
+
+        # Compare yield vs. offset
         if asset.dividend_yield is not None and asset.dividend_yield >= asset.dividend_yield_offset:
             asset.dividend_yield_signal = "sufficient"
         else:
