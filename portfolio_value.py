00f6c1b32260437fc2a833fd7e822a15bd780c40
T-Nai <tananusin@gmail.com>
2026-03-04 12:53:14 +0700

    Remove enrich_asset and enrich_assets functions
    Removed enrich_asset and enrich_assets functions to simplify asset processing.

--- portfolio_value.py
+++ portfolio_value.py
@@ -1,8 +1,6 @@
 # portfolio_value.py
 from typing import List
 from asset_data import AssetData
-from fetch_yfinance import get_price, get_fx_to_thb, get_52_week_high, get_52_week_low, get_trailing_pe, get_trailing_dividend_yield
-
 
 def calculate_asset_values(asset: AssetData) -> None:
     """
@@ -11,40 +9,6 @@ def calculate_asset_values(asset: AssetData) -> None:
     if asset.price is not None and asset.fx_rate is not None:
         asset.value_local = asset.shares * asset.price
         asset.value_thb = asset.value_local * asset.fx_rate
-
-
-def enrich_asset(asset: AssetData) -> AssetData:
-    """
-    Fetch price and fx_rate for an asset and compute its values.
-    """
-    # FX rate
-    if asset.currency == 'THB':
-        asset.fx_rate = 1
-    else:
-        asset.fx_rate = get_fx_to_thb(asset.currency)
-
-    # Price handling
-    if asset.symbol == 'CASH':
-        asset.price = 1
-    elif asset.symbol == 'BOND':
-        pass  # Use user-assigned value
-    elif asset.symbol == 'FUNDTH':
-        pass  # Use user-assigned value
-    else:
-        asset.price = get_price(asset.symbol)
-        asset.high_52w = get_52_week_high(asset.symbol)
-        asset.low_52w = get_52_week_low(asset.symbol)
-        asset.pe_ratio = get_trailing_pe(asset.symbol)
-        asset.dividend_yield = get_trailing_dividend_yield(asset.symbol)
-
-    # Calculate value
-    calculate_asset_values(asset)
-    return asset
-
-
-def enrich_assets(assets: List[AssetData]) -> List[AssetData]:
-    return [enrich_asset(asset) for asset in assets]
-
 
 def summarize_assets(assets: List[AssetData]) -> List[AssetData]:
     """
