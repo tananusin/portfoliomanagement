fc97ebd67c49dc1f4da2b1a993f96bd9573b0f00
T-Nai <tananusin@gmail.com>
2026-03-05 20:41:12 +0700

    Update risk_class.py

--- risk_class.py
+++ risk_class.py
@@ -5,5 +5,6 @@ from dataclasses import dataclass
 class RiskClass:
     name: str
     class_mdd: float
-    class_target_weight: float | None = None
     class_mdd_inverse: float | None = None
+    class_target_weight: float | None = None
+  
