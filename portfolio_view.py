2fa88aba986e463bd12efd882c3192a941f1c10a
T-Nai <tananusin@gmail.com>
2026-03-05 16:03:45 +0700

    Update portfolio_view.py

--- portfolio_view.py
+++ portfolio_view.py
@@ -19,7 +19,7 @@ def get_portfolio_df(assets: List[AssetData]) -> pd.DataFrame:
         "MDD": asset.mdd,        
         "Weight": asset.weight,
         "Target": asset.target,
-        "%drift": asset.drift_pct,
+        "%Drift": asset.drift_relative,
         "Position": asset.position_size,
         "52w high": asset.high_52w,
         "52w low": asset.low_52w,
