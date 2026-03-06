1a9a9a22e757c8279906fa2cf1f86c32b8b1ec35
T-Nai <tananusin@gmail.com>
2026-03-06 11:36:41 +0700

    Update streamlit_portfolio_management_app.py

--- streamlit_portfolio_management_app.py
+++ streamlit_portfolio_management_app.py
@@ -76,6 +76,8 @@ with tab6:
     st.subheader("💹 Google Sheet Data")
     show_google_sheet_data_table(portfolio_df)
     st.caption(f'ℹ️ "Years low" shows the lowest market price in the last {user_pref.years_rebound} years.')
+    st.caption(f'ℹ️ "PE p25" shows the PE ratio 25th percentile in the last {user_pref.years_rebound} years.')
+    st.caption(f'ℹ️ "PE p75" shows the PE ratio 75th percentile in the last {user_pref.years_rebound} years.')
 
 
 
