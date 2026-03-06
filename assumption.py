71e83a1ce0ca76333a01ee82e6a1806cd7284e0e
T-Nai <tananusin@gmail.com>
2026-03-06 13:33:04 +0700

    Update assumption.py

--- assumption.py
+++ assumption.py
@@ -1,30 +1,31 @@
 # assumption.py
 
-from typing import Optional
 from asset_data import AssetData
 from user_preferences import UserPreference
 
 
-def calculate_assumptions(asset: AssetData, user_pref: UserPreference) -> None:
-    # Calculate future return assumptions for an asset. Results are stored directly inside AssetData.
-
-    if asset.price is None:
-        return
+def calculate_assumption(asset: AssetData, user_pref: UserPreference) -> None:
+    """Calculate assumption metrics for a single asset"""
 
     # Rebound
-    if asset.fair_price is not None:
+    if asset.price and asset.fair_price:
         asset.rebound = asset.fair_price / asset.price - 1
 
     # CAGR
     if asset.rebound is not None:
-
         years = user_pref.years_rebound
-
         asset.cagr = (1 + asset.rebound) ** (1 / years) - 1
 
     # Dividend Yield Offset
-    if asset.dividend_yield is not None and user_pref.risk_free_rate is not None:
-
+    if asset.dividend_yield and user_pref.risk_free_rate:
         asset.dividend_yield_offset = (
             asset.dividend_yield - user_pref.risk_free_rate
         )
+
+
+def calculate_assumptions(assets: list[AssetData], user_pref: UserPreference) -> None:
+    """
+    Calculate assumptions for the entire portfolio
+    """
+    for asset in assets:
+        calculate_assumption(asset, user_pref)
