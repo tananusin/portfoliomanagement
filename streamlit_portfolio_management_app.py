1a5f557acfd165ff21da25492cb39a03d794613b
T-Nai <tananusin@gmail.com>
2026-03-07 08:30:45 +0700

    Update streamlit_portfolio_management_app.py

--- streamlit_portfolio_management_app.py
+++ streamlit_portfolio_management_app.py
@@ -19,7 +19,6 @@ from position_size import assign_position_sizes
 from price_change import assign_price_changes
 from yield_signal import assign_yield_signals
 from portfolio_view import get_portfolio_df, show_debug_table, show_portfolio_table, show_google_sheet_data_table, show_summary_signal_table, show_price_change_table, show_pe_signal_table, show_yield_signal_table, show_allocation_pie_chart, show_target_allocation_pie_chart, show_risk_class_table
-from pe_percentile import display_pe_percentiles
 
 
 # --- Streamlit Page Config ---
