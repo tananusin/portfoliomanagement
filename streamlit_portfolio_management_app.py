315ce3196117cce3161ba21e2d6880509b662ed6
T-Nai <tananusin@gmail.com>
2026-03-13 15:58:47 +0700

    Update streamlit_portfolio_management_app.py

--- streamlit_portfolio_management_app.py
+++ streamlit_portfolio_management_app.py
@@ -90,8 +90,8 @@ with tab3:
     st.subheader("💹 Price Signal")
     show_price_signal_table(portfolio_df)
     st.caption(f"""
-    ℹ️ "Years low" shows the lowest market price in the last {user_pref.years_rebound} years. Calmar ratio = annualized return over the past {user_pref.years_rebound} years divided by the assumed MDD. 
-    ℹ️ "PE p25" shows the PE ratio 25th percentile in the last {user_pref.years_rebound} years. Don't use Calmar ratio when asset's price crashes  
+    ℹ️ "Years low" shows the lowest market price in the last {user_pref.years_rebound} years.
+    ℹ️ "PE p25" shows the PE ratio 25th percentile in the last {user_pref.years_rebound} years. 
     """)
     st.caption(f"""
     ℹ️ Calmar ratio = annualized return over the past {user_pref.years_rebound} years divided by the assumed MDD.
