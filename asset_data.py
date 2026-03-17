e41162e122ccd436b2aa5ed1a9561004a81f9a86
T-Nai <tananusin@gmail.com>
2026-03-17 13:50:11 +0700

    Update asset_data.py

--- asset_data.py
+++ asset_data.py
@@ -46,6 +46,7 @@ class AssetData:
     price_signal: Optional[str] = None
 
     # P/E Signal
+    eps: Optional[float] = None                         # Trailing EPS  
     pe_ratio: Optional[float] = None                    # Trailing P/E ratio
     pe_p25: Optional[float] = None                      # 25th percentile P/E
     pe_p75: Optional[float] = None                      # 75th percentile P/E
