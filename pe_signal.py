a9f2db350ee59220bfd2fc4f88d1e7a90be9ba2f
T-Nai <tananusin@gmail.com>
2026-03-17 13:58:28 +0700

    Update pe_signal.py

--- pe_signal.py
+++ pe_signal.py
@@ -1,19 +1,31 @@
-#pe_signal.py
+# pe_signal.py
 from asset_data import AssetData
 from typing import List
 
 def assign_pe_signals(assets: List[AssetData]) -> None:
     for asset in assets:
+
+        # --- Step 1: Calculate PE from EPS ---
+        if asset.eps is not None and asset.eps != 0 and asset.price is not None:
+            asset.pe_ratio = asset.price / asset.eps
+        else:
+            asset.pe_ratio = None
+
+        # --- Step 2: Assign Signal ---
         if asset.pe_ratio is None or asset.pe_ratio == 0:
             asset.pe_signal = None
+
         elif (
             asset.pe_p25 is None or asset.pe_p25 == 0 or
             asset.pe_p75 is None or asset.pe_p75 == 0
         ):
             asset.pe_signal = "no data"
+
         elif asset.pe_ratio < asset.pe_p25:
             asset.pe_signal = "undervalue"
+
         elif asset.pe_ratio > asset.pe_p75:
             asset.pe_signal = "overvalue"
+
         else:
             asset.pe_signal = "-"
