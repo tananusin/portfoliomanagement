246f4a9c85c06c2dc29bfd25398d5b4cd5ae78b0
T-Nai <tananusin@gmail.com>
2026-03-17 13:52:36 +0700

    Update asset_data.py

--- asset_data.py
+++ asset_data.py
@@ -13,6 +13,8 @@ class AssetData:
     fx_rate: Optional[float] = None
     asset_class: Optional[str] = None
     mdd: Optional[float] = None
+    eps: Optional[float] = None                         # Trailing EPS
+    dividend_yield: Optional[float] = None              # Trailing dividend yield
     
     # Portfolio Value Variables
     value_local: Optional[float] = None
@@ -46,12 +48,10 @@ class AssetData:
     price_signal: Optional[str] = None
 
     # P/E Signal
-    eps: Optional[float] = None                         # Trailing EPS  
     pe_ratio: Optional[float] = None                    # Trailing P/E ratio
     pe_p25: Optional[float] = None                      # 25th percentile P/E
     pe_p75: Optional[float] = None                      # 75th percentile P/E
     pe_signal: Optional[str] = None
 
     # Dividend Yield Signal
-    dividend_yield: Optional[float] = None              # Trailing dividend yield
     dividend_yield_signal: Optional[str] = None
