b82c39c08961386d19fcf7fcd202fe5154676b5c
T-Nai <tananusin@gmail.com>
2026-08-30 10:19:30 +0700

    Update user_preferences.py

--- user_preferences.py
+++ user_preferences.py
@@ -114,8 +114,8 @@ def get_user_preferences() -> UserPreference:
     assumed_mdd = assumed_mdd_pct / 100
     assumed_rebound = 1 / (1 - assumed_mdd) - 1
     assumed_cagr = (1 + assumed_rebound) ** (1 / years_rebound) - 1
-    st.sidebar.caption(f"Assumed Rebound Rate: {assumed_rebound:.1%}")
-    st.sidebar.caption(f"Required CAGR over {int(years_rebound)} years: {assumed_cagr:.1%}")
+    st.sidebar.metric(f"Assumed Rebound Rate: {assumed_rebound:.1%}")
+    st.sidebar.metric(f"Required CAGR over {int(years_rebound)} years: {assumed_cagr:.1%}")
     
     # Convert % to decimals
     investment_weight = investment_pct / 100
